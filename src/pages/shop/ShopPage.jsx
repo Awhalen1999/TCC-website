de6540fe3c187c83e0cdde747b03ade888d87227
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-03-14 00:06:52 -0230

    modal working shop page

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -22,49 +22,87 @@ const ShopPage = () => {
         </div>
       </div>
       {/* end of alert */}
-      <h1 className='text-3xl font-bold mb-4'>Create your custom rug</h1>
-      <h2 className='text-2xl font-semibold mb-4'>How it works</h2>
-      {/* steps */}
-      <ul className='steps steps-vertical'>
-        <li className='step'>Choose a rug sizing</li>
-        <li className='step'>Upload an image</li>
-        <li className='step'>Add design specifications</li>
-        <li className='step'>Wait for email</li>
-      </ul>
-      {/* end of steps */}
-      <div className='overflow-x-auto'>
-        <table className='table w-full'>
-          {/* head */}
-          <thead>
-            <tr>
-              <th>Sizing</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1x1</th>
-              <td>125$</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>2x2</th>
-              <td>200$</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3x3</th>
-              <td>300$</td>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <th>4x4</th>
-              <td>400$</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <h1 className='text-3xl font-bold mb-4 text-center'>CUSTOM ORDER FORM</h1>
+      <h2 className='text-lg mb-4'>
+        Thanks for considering a custom order with us! Crafting these custom
+        rugs takes time and care. If you're curious about pricing before diving
+        in, feel free to check it out{' '}
+        <button
+          className='link link-info'
+          onClick={() => document.getElementById('my_modal_1').showModal()}
+        >
+          HERE
+        </button>
+        .
+      </h2>
+
+      <dialog id='my_modal_1' className='modal'>
+        <div className='modal-box'>
+          <h1 className='text-3xl font-bold mb-4'>Starting Prices</h1>
+          <div className='overflow-x-auto border rounded-xl bg-white w-[30vw]'>
+            <table className='table w-full'>
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Sizing</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>1' x 1'</th>
+                  <td>125$</td>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <th>2' x 2'</th>
+                  <td>200$</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <th>3' x 3'</th>
+                  <td>300$</td>
+                </tr>
+                {/* row 4 */}
+                <tr>
+                  <th>4' x 4'</th>
+                  <td>400$</td>
+                </tr>
+                {/* row 5 */}
+                <tr>
+                  <th>5' x 5'</th>
+                  <td>500$</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='modal-action'>
+            <form method='dialog'>
+              {/* if there is a button in form, it will close the modal */}
+              <button className='btn'>Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
+      <h2 className='text-lg mb-4'>
+        We want to be upfront with you: it might take anywhere from 2 to 5 weeks
+        to finish your order. It all depends on the size, detail, and how many
+        orders we've got lined up before yours. But don't worry, we'll keep you
+        in the loop every step of the way!
+      </h2>
+
+      <h2 className='text-lg mb-4'>
+        Oh, and one more thing: the starting prices can vary depending on how
+        intricate the design is and the colors you choose. Thanks for
+        understanding!
+      </h2>
+
+      <h2 className='text-lg mb-4'>
+        Looking forward to creating something special with you!
+      </h2>
+
       <h1 className='text-3xl font-bold mb-4 mt-8'>FAQ</h1>
       <div
         tabIndex={0}
